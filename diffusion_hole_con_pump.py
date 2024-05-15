a37bfe4d2dffc3facece2f94204013803d61bda5
xxavieryang <80008482+xxavieryang@users.noreply.github.com>
2024-05-15 11:11:06 +0200

    Update diffusion_hole_con_pump.py

--- diffusion_hole_con_pump.py
+++ diffusion_hole_con_pump.py
@@ -1,1 +1,6 @@
-import 
+from mpi4py import MPI
+from dolfinx import mesh
+from dolfinx.fem import functionspace
+domain = mesh.create_unit_square(MPI.COMM_WORLD,10,10,mesh.CellType.triangle)
+V=functionspace(domain,("Lagrange",1))
+from  import
