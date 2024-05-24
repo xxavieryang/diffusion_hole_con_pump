0fc9ba07a961639752dfd73190b4718179befdca
xxavieryang <xiao.xavier.yang@gmail.com>
2024-05-24 19:44:43 +0200

    point source change

--- diffusion_hole_con_pump.py
+++ diffusion_hole_con_pump.py
@@ -34,7 +34,7 @@ dt = T / num_steps  # Time step size
 
 #Define the Petri dish and the cells
 if mesh_comm.rank == model_rank:
-	dish = gmsh.model.occ.addRectangle(0, 0, 0, L, W, tag=1)
+	dish = gmsh.model.occ.addRectangle(0, 0, 0, L, W, tag=1)	
 	cell1 = gmsh.model.occ.addDisk(c_x, c_y, 0, r, r)
 	cell2 = gmsh.model.occ.addDisk(c_x-2, c_y-3, 0, r, r)
 	#cell3 = gmsh.model.occ.addDisk(c_x-6, c_y-6, 0, r, r)
@@ -42,26 +42,28 @@ if mesh_comm.rank == model_rank:
 
 #Cut out for spatial exclusion model
 if  mesh_comm.rank == model_rank:
-	spatial_exclusion_domain = gmsh.model.occ.cut([(gdim,dish)],[(gdim,cell1)])
-	spatial_exclusion_domain = gmsh.model.occ.cut([(gdim,dish)],[(gdim,cell2)])
-	#spatial_exclusion_domain = gmsh.model.occ.cut([(gdim,dish)],[(gdim,cell3)])
+	gmsh.model.occ.cut([(gdim,dish)],[(gdim,cell1)])
+	gmsh.model.occ.cut([(gdim,dish)],[(gdim,cell2)])
+	#gmsh.model.occ.cut([(gdim,dish)],[(gdim,cell3)])
 	gmsh.model.occ.synchronize()
 
+
 #Spatial exclusion mesh with more refined meshing around the cells
-liquid_marker = 1
+liquid_marker_s = 1
 if mesh_comm.rank == model_rank:
-	volumes = gmsh.model.getEntities(dim=gdim)
-	print(len(volumes))
-	assert(len(volumes) == 1)
-	gmsh.model.addPhysicalGroup(volumes[0][0],[volumes[0][1]],liquid_marker)
-	gmsh.model.setPhysicalName(volumes[0][0],liquid_marker,"Liquid")
+	volume_s = gmsh.model.getEntities(dim=gdim)
+	print(len(volume_s))
+	assert(len(volume_s) == 1)
+	gmsh.model.addPhysicalGroup(volume_s[0][0],[volume_s[0][1]],liquid_marker_s)
+	gmsh.model.setPhysicalName(volume_s[0][0],liquid_marker_s,"Liquid_s")
+
 
 wall_marker, cells_marker = 2, 3
 
 wall, cells = [], []
 
 if mesh_comm.rank == model_rank:
-	boundaries = gmsh.model.getBoundary(volumes, oriented = False)
+	boundaries = gmsh.model.getBoundary(volume_s, oriented = False)
 	for boundary in boundaries:
 		center_of_mass = gmsh.model.occ.getCenterOfMass(boundary[0],boundary[1])
 		if np.allclose(center_of_mass,[L/2,0,0]) or np.allclose(center_of_mass,[0,W/2,0]) or \
@@ -93,8 +95,8 @@ if mesh_comm.rank == model_rank:
 	gmsh.model.mesh.setOrder(1)
 	gmsh.model.mesh.optimize("Netgen")
 
-domain, cell_markers, facet_markers = gmshio.model_to_mesh(gmsh.model, mesh_comm, model_rank, gdim = gdim)
-V = functionspace(domain, ("Lagrange", 1))
+domain_spatial_exclusion, cell_markers, facet_markers = gmshio.model_to_mesh(gmsh.model, mesh_comm, model_rank, gdim = gdim)
+V_s = functionspace(domain_spatial_exclusion, ("Lagrange", 1))
 
 
 #Show mesh
@@ -102,9 +104,56 @@ print(pyvista.global_theme.jupyter_backend)
 
 from dolfinx import plot
 
-topology, cell_types, geometry = plot.vtk_mesh(V)
+topology, cell_types, geometry = plot.vtk_mesh(V_s)
 grid = pyvista.UnstructuredGrid(topology, cell_types, geometry)
 plotter = pyvista.Plotter()
+plotter.add_mesh(grid, color = [1.0,1.0,1.0], show_edges = True)
+plotter.view_xy()
+if not pyvista.OFF_SCREEN:
+    plotter.show()
+else:
+    figure = plotter.screenshot("fundamentals_mesh.png")
+
+if  mesh_comm.rank == model_rank:
+	gmsh.model.occ.addRectangle(0, 0, 0, L, W, tag=2)
+	gmsh.model.occ.synchronize()
+
+if mesh_comm.rank == model_rank:
+	gmsh.model.mesh.generate(gdim)
+	gmsh.model.mesh.setOrder(1)
+	gmsh.model.mesh.optimize("Netgen")
+
+
+#Add the hole back
+if  mesh_comm.rank == model_rank:
+	cell1b = gmsh.model.occ.addDisk(c_x, c_y, 0, r, r)
+	cell2b = gmsh.model.occ.addDisk(c_x-2, c_y-3, 0, r, r)
+	#cell3b = gmsh.model.occ.addDisk(c_x-6, c_y-6, 0, r, r)
+	gmsh.model.occ.fuse([(gdim,dish)],[(gdim,cell1b)])
+	gmsh.model.occ.fuse([(gdim,dish)],[(gdim,cell2b)])
+	#gmsh.model.occ.fuse([(gdim,dish)],[(gdim,cell3b)])
+	gmsh.model.occ.synchronize()
+
+liquid_marker_p = 4
+if mesh_comm.rank == model_rank:
+	volume_p = gmsh.model.getEntities(dim=gdim)
+	print(len(volume_p))
+	assert(len(volume_p) == 2)
+	gmsh.model.addPhysicalGroup(volume_p[0][0],[volume_p[0][1]],liquid_marker_p)
+	gmsh.model.setPhysicalName(volume_p[0][0],liquid_marker_p,"Liquid_p")
+
+if mesh_comm.rank == model_rank:
+	gmsh.model.mesh.generate(gdim)
+	gmsh.model.mesh.setOrder(1)
+	gmsh.model.mesh.optimize("Netgen")
+
+domain_point_source, cell_markers, facet_markers = gmshio.model_to_mesh(gmsh.model, mesh_comm, model_rank, gdim = gdim)
+V_p = functionspace(domain_point_source, ("Lagrange", 1))
+
+topology, cell_types, geometry = plot.vtk_mesh(V_p)
+grid = pyvista.UnstructuredGrid(topology, cell_types, geometry)
+plotter = pyvista.Plotter()
+
 plotter.add_mesh(grid, color = [1.0,1.0,1.0], show_edges = True)
 plotter.view_xy()
 if not pyvista.OFF_SCREEN:
@@ -118,10 +167,11 @@ def initial_condition(x, disp=1,cct=2):
     return np.exp(-disp*((x[0]-7)**2+(x[1]-7)**2)+cct)
 
 
+"""
 #Boundary markers
-fdim = domain.topology.dim - 1
+fdim = domain_spatial_exclusion.topology.dim - 1
 #boundary_facets = mesh.locate_entities_boundary(
-#    domain, fdim, lambda x: np.full(x.shape[1], True, dtype=bool))
+#    domain_spatial_exclusion, fdim, lambda x: np.full(x.shape[1], True, dtype=bool))
 #bc = fem.dirichletbc(PETSc.ScalarType(0), fem.locate_dofs_topological(V, fdim, boundary_facets), V)
 
 boundary_locator = [(1, lambda x: np.isclose(x[0], 0)),
@@ -133,21 +183,21 @@ boundary_locator = [(1, lambda x: np.isclose(x[0], 0)),
 
 facet_indices, facet_markers = [], []
 for (marker, locator) in boundary_locator:
-    facets = mesh.locate_entities(domain, fdim, locator)
+    facets = mesh.locate_entities(domain_spatial_exclusion, fdim, locator)
     facet_indices.append(facets)
     facet_markers.append(np.full_like(facets, marker))
 facet_indices = np.hstack(facet_indices).astype(np.int32)
 facet_markers = np.hstack(facet_markers).astype(np.int32)
 sorted_facets = np.argsort(facet_indices)
-facet_tag = mesh.meshtags(domain, fdim, facet_indices[sorted_facets], facet_markers[sorted_facets])
+facet_tag = mesh.meshtags(domain_spatial_exclusion, fdim, facet_indices[sorted_facets], facet_markers[sorted_facets])
 
-ds = Measure("ds", domain=domain, subdomain_data=facet_tag)
+ds = Measure("ds", domain=domain_spatial_exclusion, subdomain_data=facet_tag)
 
 
 #Spatial exclusion model computation
-u_s = TrialFunction(V)
-v_s = TestFunction(V)
-u_sn = Function(V)
+u_s = TrialFunction(V_s)
+v_s = TestFunction(V_s)
+u_sn = Function(V_s)
 u_sn.interpolate(initial_condition)
 
 a_s = u_s * v_s * dx + dt * D * dot(grad(u_s), grad(v_s)) * dx + dt * a * u_s * v_s * ds(5) + dt * a * u_s * v_s * ds(6)
@@ -163,14 +213,14 @@ A_s = assemble_matrix(bilinearform_s)
 A_s.assemble()
 b_s = create_vector(linearform_s)
 
-solver = PETSc.KSP().create(domain.comm)
+solver = PETSc.KSP().create(domain_spatial_exclusion.comm)
 solver.setOperators(A_s)
 solver.setType(PETSc.KSP.Type.PREONLY)
 solver.getPC().setType(PETSc.PC.Type.LU)
 
-u_s = Function(V)
+u_s = Function(V_s)
 
-grid = pyvista.UnstructuredGrid(*plot.vtk_mesh(V))
+grid = pyvista.UnstructuredGrid(*plot.vtk_mesh(V_s))
 
 plotter = pyvista.Plotter()
 plotter.open_gif("spatial_exclusion.gif", fps=10)
@@ -186,8 +236,8 @@ renderer = plotter.add_mesh(grid, show_edges=True, lighting=False,
                             cmap=viridis, scalar_bar_args=sargs,
                             clim=[0, max(u_s.x.array)])
 
-xdmf = io.XDMFFile(domain.comm, "spatial_exclusion.xdmf", "w")
-xdmf.write_mesh(domain)
+xdmf = io.XDMFFile(domain_spatial_exclusion.comm, "spatial_exclusion.xdmf", "w")
+xdmf.write_mesh(domain_spatial_exclusion)
 
 for i in range(num_steps):
 	t += dt
@@ -207,3 +257,36 @@ for i in range(num_steps):
 	plotter.write_frame()
 plotter.close()
 xdmf.close()
+"""
+
+#Point source computation
+fdim = domain_spatial_exclusion.topology.dim - 1
+#boundary_facets = mesh.locate_entities_boundary(
+#    domain_spatial_exclusion, fdim, lambda x: np.full(x.shape[1], True, dtype=bool))
+#bc = fem.dirichletbc(PETSc.ScalarType(0), fem.locate_dofs_topological(V, fdim, boundary_facets), V)
+
+u_p = TrialFunction(V_p)
+v_p = TestFunction(V_p)
+u_pn = Function(V_p)
+u_pn.interpolate(initial_condition)
+
+
+subdomain_locator
+
+subdomain_locator = [(1, lambda x: (x[0]-c_x)**2+(x[1]-c_y)**2<=r**2  (x[0]-c_x)**2+(x[1]-c_y)**2<=r**2),
+              (2, lambda x: (x[0]-c_x+2)**2+(x[1]-c_y+3)**2<=r**2),
+              (3, lambda x: x[0]>=9)]
+
+print("2")
+
+facet_indices, facet_markers = [], []
+for (marker, locator) in subdomain_locator:
+    facets = mesh.locate_entities(domain_spatial_exclusion, gdim, locator)
+    facet_indices.append(facets)
+    facet_markers.append(np.full_like(facets, marker))
+facet_indices = np.hstack(facet_indices).astype(np.int32)
+facet_markers = np.hstack(facet_markers).astype(np.int32)
+sorted_facets = np.argsort(facet_indices)
+facet_tag = mesh.meshtags(domain_spatial_exclusion, fdim, facet_indices[sorted_facets], facet_markers[sorted_facets])
+
+dx = Measure("dx", domain=domain_point_source, subdomain_data=facet_tag)
